5ae62cb8cbfff4f4c5ef648a447fc60de8bd0376
Navneet <navn0306@gmail.com>
2021-11-27 17:53:31 +0530

    ADD: Added program to calculate the power of 2

--- src/com/bridge/Power/Power.java
+++ src/com/bridge/Power/Power.java
@@ -1,7 +1,21 @@
 package com.bridge.Power;
 
+import java.util.Scanner;
+
 public class Power {
+    private static Scanner sc;
+
     public static void main(String[] args) {
-        System.out.println(" \n" + " Welcome, This program prints the power of 2");
+        int number = 2, i, exponent;
+        long power = 1;
+        sc = new Scanner(System.in);
+
+        System.out.print(" Please Enter the Exponent Value : ");
+        exponent = sc.nextInt();
+
+        for (i = 1; i <= exponent; i++) {
+            power = power * number;
+        }
+        System.out.println("\n The Final result of " + number + " power " + exponent + " = " + power);
     }
 }

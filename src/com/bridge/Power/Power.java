26832d4c23fa2e1f1a75c6c6be3db0d9e6c04fa8
Navneet <navn0306@gmail.com>
2021-11-27 17:49:23 +0530

    Add: Added Welcome message

---
+++ src/com/bridge/Power/Power.java
@@ -0,0 +1,7 @@
+package com.bridge.Power;
+
+public class Power {
+    public static void main(String[] args) {
+        System.out.println(" \n" + " Welcome, This program prints the power of 2");
+    }
+}
